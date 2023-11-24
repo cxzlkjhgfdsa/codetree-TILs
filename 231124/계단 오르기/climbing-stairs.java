9def92fb23d65f3c7cc581ca4781eb3e72ea81be
cxzlkjhgfdsa <96561194+cxzlkjhgfdsa@users.noreply.github.com>
2023-11-24 18:10:14 +0900

    Title: 계단 오르기, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231124/계단 오르기/climbing-stairs.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] dp = new int[n+2];
+
+        dp[2] = 1;
+        dp[3] = 1;
+        if(n >= 4){
+
+            for(int i = 4; i <= n; i++){
+                dp[i] += dp[i-2];
+                dp[i] += dp[i-3];
+            }
+        }
+        
+        System.out.println(dp[n]);
+    }
+}
