a1f73d4b32989b169147172dd2abf1bf871052a3
cxzlkjhgfdsa <96561194+cxzlkjhgfdsa@users.noreply.github.com>
2023-12-03 03:56:02 +0900

    Title: 동전 거슬러주기, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231203/동전 거슬러주기/coin-change.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[] coins = new int[N];
+        int[] dp = new int[20001];
+
+        Arrays.fill(dp, 10000);
+
+        for(int i = 0; i < N; i++){
+            coins[i]  = sc.nextInt();
+        }
+
+        dp[0] = 0;
+
+        for(int i = 0; i <= 10000; i++){
+            for(int j : coins){
+                dp[i+j] = Math.min(dp[i+j] , dp[i] + 1);
+            }
+        }
+
+        System.out.println(dp[M]);
+
+
+    }
+}
