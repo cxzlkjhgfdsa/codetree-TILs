fb55d20bfe084e27110ff9775264176d4e0d499e
cxzlkjhgfdsa <96561194+cxzlkjhgfdsa@users.noreply.github.com>
2023-12-19 12:22:28 +0900

    Title: 양수 직사각형의 최대 크기, Time: 147ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231219/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class Main {
+
+    static int n, m, ans;
+    static int[][] area;
+    static int[] nums, secNums;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        ans = -1;
+
+        area = new int[n][m];
+        nums = new int[2];
+        secNums = new int[2];
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                area[i][j] = sc.nextInt();
+            }
+        }
+
+        combi(0, 0);
+
+        System.out.println(ans);
+
+    }
+    
+    static void makeSquare(int[] nums, int cnt, int start){
+        if(cnt == 2){
+            
+            boolean isPlus = true;
+            int count = 0;
+
+            for(int i = nums[0]; i <=nums[1]; i++){
+                for(int j = secNums[0]; j <= secNums[1]; j++){
+                    if(area[i][j] < 0)
+                        isPlus = false;
+                    count++;
+                }
+            }
+
+            if(isPlus){
+                ans = Math.max(count, ans);
+            }
+            return;
+        }
+        
+        for(int i = start; i < n; i++){
+            secNums[cnt] = i;
+            makeSquare(nums, cnt+1, i);
+        }
+    }
+
+    static void combi(int cnt, int start){
+        if(cnt == 2){
+            makeSquare(nums , 0, 0);
+            return;
+        }
+        
+        for(int i = start; i < n; i++){
+            nums[cnt] = i;
+            combi(cnt+1, i);
+        }
+    }
+}
