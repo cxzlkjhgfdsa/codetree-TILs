c2c8d8bbe93e04d130d73484d0fdf788d802fa7d
cxzlkjhgfdsa <96561194+cxzlkjhgfdsa@users.noreply.github.com>
2024-04-02 07:53:30 +0900

    Title: xor 결과 최대 만들기, Time: 201ms, Memory: 10MB, Status: Passed - CodeTree

--- 240402/xor 결과 최대 만들기/max-of-xor.java
+++ 240402/xor 결과 최대 만들기/max-of-xor.java
@@ -1,44 +1,55 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-
-    static List<Integer> list;
-    static int[] nums;
-    static int max;
+    public static final int MAX_NUM = 20;
+    
+    // 전역 변수 선언:
+    public static int n, m;
+    public static int[] A = new int[MAX_NUM];
+    public static boolean[] visited = new boolean[MAX_NUM];
+    
+    public static int ans;
+    
+    public static int calc() {
+        // xor 연산의 항등원인 0을 초기값으로 설정합니다.
+        int val = 0;
+        for(int i = 0; i < n; i++)
+            if(visited[i])
+                val ^= A[i];
+        
+        return val;
+    }
+    
+    public static void findMaxXor(int currIdx, int cnt) {
+        if(cnt == m) {
+            // 선택된 모든 조합에 대해 xor 연산을 적용해봅니다.
+            ans = Math.max(ans, calc());
+            return;
+        }
+    
+        if(currIdx == n) 
+            return;
+    
+        // currIdx index 에 있는 숫자를 선택하지 않은 경우
+        findMaxXor(currIdx + 1, cnt);
+        
+        // currIdx index 에 있는 숫자를 선택한 경우
+        visited[currIdx] = true;
+        findMaxXor(currIdx + 1, cnt + 1);
+        visited[currIdx] = false;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        max = 0;
-        list = new ArrayList<>();
+        // 입력:
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(int i = 0; i < n; i++) 
+            A[i] = sc.nextInt();
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-
-        nums = new int[n];
-        for(int i = 0; i < n; i++){
-            nums[i] = sc.nextInt();
-        }
-
-        comb(n, m, 0, 0);
-
-        System.out.println(max);
-    }
-
-    private static void comb(int n, int m, int cnt, int start){
-        if(cnt == m){
-            int xor = list.get(0);
-            for(int i = 1; i < list.size(); i++){
-                xor ^= list.get(i);
-            }
-            max = Math.max(max, xor);
-            
-        }
-
-
-        for(int i = start; i < n; i++){
-            list.add(nums[i]);
-            comb(n, m, cnt+1, i+1);
-            list.remove(list.size() - 1);
-        }
+        findMaxXor(0, 0);
+        
+        // 출력:
+        System.out.print(ans);
     }
 }
