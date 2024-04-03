d5951a5d3ad42efb1ff3eb16c2786cf5c5ececec
cxzlkjhgfdsa <96561194+cxzlkjhgfdsa@users.noreply.github.com>
2024-04-03 21:06:31 +0900

    Title: 거꾸로 순열, Time: 1632ms, Memory: 50MB, Status: Passed - CodeTree

---
+++ 240403/거꾸로 순열/backward-permutation.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+
+    static int [] nums;
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+        nums = new int[n];
+        visited = new boolean[n+1];
+
+        perm(n, 0);
+        
+    }
+
+    private static void perm(int n, int cnt){
+
+        if(cnt == n){
+
+            for(Integer i : nums){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+            return;
+        }
+
+
+        for(int i = n; i >= 1; i--){
+            
+            if(visited[i])
+                continue;
+            visited[i] = true;
+            nums[cnt] = i;
+
+            perm(n, cnt+1);
+            visited[i] = false;
+        }
+    }
+}
