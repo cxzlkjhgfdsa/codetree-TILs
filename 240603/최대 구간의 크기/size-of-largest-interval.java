4f26cb3c52828861e457c91f231bfabbb9e1c4a8
cxzlkjhgfdsa <96561194+cxzlkjhgfdsa@users.noreply.github.com>
2024-06-03 18:13:13 +0900

    Title: 최대 구간의 크기, Time: 843ms, Memory: 35MB, Status: Wrong Answer - CodeTree

---
+++ 240603/최대 구간의 크기/size-of-largest-interval.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        List<Point> list = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            list.add(new Point(sc.nextInt(), 1));
+            list.add(new Point(sc.nextInt(), -1));
+        }
+
+        Collections.sort(list);
+
+        int max = 0;
+        int sum = 0;
+        int sumVal = 0;
+
+        sumVal += list.get(0).v;
+
+        for(int i = 1; i < list.size(); i++){
+            sumVal += list.get(i).v;
+            sum += list.get(i).s - list.get(i-1).s;
+
+            //System.out.println(sumVal);
+
+            if(sumVal == 0){
+                max = Math.max(max, sum);
+                sum = 0;
+            }
+        }
+
+        System.out.println(max);
+
+    }
+
+    private static class Point implements Comparable<Point>{
+        int s, v;
+        public Point(int s, int v){
+            this.s = s;
+            this.v = v;
+        }
+
+        @Override
+        public int compareTo(Point p){
+            return this.s - p.s;
+        }
+    }
+}
