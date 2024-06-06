fa9c58e664af769277496810111c79f4b7326f4b
cxzlkjhgfdsa <96561194+cxzlkjhgfdsa@users.noreply.github.com>
2024-06-06 23:15:17 +0900

    Title: 겹치는 원소가 적은 부분 수열, Time: 706ms, Memory: 33MB, Status: Wrong Answer - CodeTree

---
+++ 240606/겹치는 원소가 적은 부분 수열/subsequences-with-few-overlapping-elements.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int start = 0;
+        int end = 0;
+
+        int max = 0;
+
+        while(start <= end && end < n){
+
+            if(map.get(arr[end]) == null || map.get(arr[end]) < k){
+                max = Math.max(max, end - start + 1);
+                if(map.get(arr[end]) == null){
+                    map.put(arr[end], 1);
+                }else{
+                    map.put(arr[end], map.get(arr[end]) + 1);
+                }
+                end++;
+            }else{
+                map.put(arr[start], map.get(arr[start]-1));
+                start++;
+            }
+
+            
+
+        }
+
+        System.out.println(max);
+
+    }
+}
